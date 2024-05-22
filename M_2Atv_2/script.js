4fc0799dd54c173375c0ad1eb58eca9d311ba3ab
victorcsar <victorcesagx@gmail.com>
2024-05-22 19:39:47 -0300

    ATT tables

--- M_2Atv_2/script.js
+++ M_2Atv_2/script.js
@@ -1,4 +1,4 @@
-document.getElementById('userForm').addEventListener('submit', function(event) {
+document.getElementById('userForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
     const name = document.getElementById('name').value;
@@ -13,9 +13,23 @@ document.getElementById('userForm').addEventListener('submit', function(event) {
     const cell1 = newRow.insertCell(0);
     const cell2 = newRow.insertCell(1);
     const cell3 = newRow.insertCell(2);
+    const cell4 = newRow.insertCell(3);
     cell1.textContent = name;
     cell2.textContent = email;
     cell3.textContent = age;
+
+    // cell1.classList.add('col1');
+    // cell2.classList.add('col2');
+    // cell3.classList.add('col3');
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Deletar';
+    deleteButton.classList.add('delete-btn');
+    cell4.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', function () {
+        table.deleteRow(newRow.rowIndex - 1);
+    });
 
     // Limpar o formulário
     document.getElementById('userForm').reset();
